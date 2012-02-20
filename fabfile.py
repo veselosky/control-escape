75b7ba43f8bae44703a6a28b8924785ff8c09358
Vince Veselosky <vince@control-escape.com>
2012-02-19 21:41:55 -0500

    account for favicon and make local tasks robuster

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,15 @@
-from fabric.api import env, local, task
+from fabric.api import env, lcd, local, task
+import os.path
 import otto.git as git
 import otto.web as web
 
 env['otto.git.remote_repo_path'] = '/home/vince/Repo'
 env['otto.web.build_dir'] = './build/staged'
 env['otto.web.site'] = 'www.control-escape.com'
+
+def _project_dir():
+    """Helper to return the project directory"""
+    return os.path.dirname(os.path.realpath(env['real_fabfile']))
 
 @task
 def B():
@@ -15,7 +20,15 @@ def B():
 def build():
     """Build the site"""
     target = env['otto.web.build_dir']
-    local('rm -rf build/*') # make clean
-    local('sphinx-build -n -d build/doctrees -b html source %s/htdocs' % target)
-    local('cp -a etc %s/' % target)
+    with lcd(_project_dir()):
+        local('rm -rf build/*') # make clean
+        local('sphinx-build -n -d build/doctrees -b html source %s/htdocs' % target)
+        local('mv %s/htdocs/_static/favicon.ico %s/htdocs/favicon.ico' % (target,target))
+        local('cp -a etc %s/' % target)
 
+@task
+def clean():
+    """Clean up stale build files and tarballs"""
+    with lcd(_project_dir()):
+        local('rm -rf build/*') # make clean
+        local('rm -f *.tar.gz') # make clean
