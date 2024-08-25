a5eb0944c68921cd948f795a2fdbe0b8ad794963
Vince Veselosky <vince@control-escape.com>
2024-08-24 20:02:01 -0400

    Cleanup settings and urls, add sitevars

--- ce/settings.py
+++ ce/settings.py
@@ -10,35 +10,37 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import importlib
 from pathlib import Path
+
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT = __name__.split(".")[0]
+# Get environment settings
+env = environ.Env()
+DOTENV = BASE_DIR / ".env"
+if DOTENV.exists() and not env("IGNORE_ENV_FILE", default=False):
+    environ.Env.read_env(DOTENV)
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1q4th^9c$20lm7zjpemk(1-vy1u#$63c9j=%1cx4o@0$9)28@u"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
+#######################################################################################
+# Application definition, settings that should not vary between environments
+#######################################################################################
+WSGI_APPLICATION = f"{PROJECT}.wsgi.application"
+ROOT_URLCONF = f"{PROJECT}.urls"
 
 INSTALLED_APPS = [
     "django.contrib.admin",
+    "django.contrib.admindocs",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.sites",
     "django.contrib.staticfiles",
+    "sitevars",
 ]
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -47,10 +49,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.contrib.sites.middleware.CurrentSiteMiddleware",
 ]
-
-ROOT_URLCONF = "ce.urls"
-
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -62,28 +62,27 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "sitevars.context_processors.inject_sitevars",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "ce.wsgi.application"
+# Internationalization
+# https://docs.djangoproject.com/en/5.1/topics/i18n/
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "America/New_York"
+USE_I18N = True
+USE_TZ = True
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -99,25 +98,114 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#######################################################################################
+# Settings that can (and maybe should) differ between environments
+#######################################################################################
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
+# SECRET_KEY intentionally has no default, and will error if not provided
+# in the environment. This ensures you don't accidentally run with an
+# insecure configuration in production.
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG", default=False)
+ALLOWED_HOSTS = env("ALLOWED_HOSTS", default=[])
 
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
-USE_TZ = True
-
+# Local data written by the app should be kept in one directory for ease of backup.
+# In DEV this can be a subdir of BASE_DIR. In production, for single-server setups
+# this should be a directory outside BASE_DIR that is backed up on a regular basis.
+# For scalable configurations, you should not use local paths but external services
+# like S3 and a dedicated database server.
+DATA_DIR = Path(env("DATA_DIR", default=BASE_DIR.joinpath("var")))
+DATA_DIR.mkdir(exist_ok=True)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = "/static/"
+STATIC_ROOT = DATA_DIR / "static"
+STATIC_ROOT.mkdir(parents=True, exist_ok=True)
+MEDIA_URL = "/media/"
+MEDIA_ROOT = DATA_DIR / "media"
+MEDIA_ROOT.mkdir(parents=True, exist_ok=True)
 
-STATIC_URL = "static/"
+# ManifestStaticFilesStorage is recommended in production, to prevent outdated
+# Javascript / CSS assets being served from cache.
+# See https://docs.djangoproject.com/en/5.1/ref/contrib/staticfiles/#manifeststaticfilesstorage
+# But for production, you almost certainly should be using a shared storage backend, like:
+# https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
+STORAGES = {
+    "default": {
+        "BACKEND": env(
+            "DEFAULT_STORAGE", default="django.core.files.storage.FileSystemStorage"
+        ),
+    },
+    "staticfiles": {
+        "BACKEND": env(
+            "STATICFILES_STORAGE",
+            default="django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+        ),
+    },
+}
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+DB_DIR = DATA_DIR / "db"
+DB_DIR.mkdir(parents=True, exist_ok=True)
+SQLITE_DB = DB_DIR / "db.sqlite3"
+DATABASES = {"default": env.db("DATABASE_URL", default=f"sqlite:///{SQLITE_DB}")}
+# TODO tweak performance settings for SQLite and pooling for Postgres
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CACHES = {"default": env.cache("CACHE_URL", default="locmemcache://")}
+
+# Email settings don't use a dict. Add to local vars instead.
+# https://django-environ.readthedocs.io/en/latest/#email-settings
+EMAIL_CONFIG = env.email_url("EMAIL_URL", default="consolemail://")
+vars().update(EMAIL_CONFIG)
+
+# CELERY settings
+# If the environment has not provided settings, assume there is no broker
+# and run celery tasks in-process. This means you MUST provide
+# CELERY_TASK_ALWAYS_EAGER=False in your environment to actually use celery.
+CELERY_TASK_ALWAYS_EAGER = env("CELERY_TASK_ALWAYS_EAGER", default=True)
+CELERY_TASK_EAGER_PROPAGATES = env("CELERY_TASK_EAGER_PROPAGATES", default=True)
+# For development setup, assume default of local redis.
+CELERY_BROKER_URL = env("CELERY_BROKER_URL", default="redis://localhost:6379/1")
+CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND", default="")
+CELERY_TIME_ZONE = TIME_ZONE
+# If celery beat is installed in the environment, use its database scheduler.
+if importlib.util.find_spec("django_celery_beat"):
+    CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+    INSTALLED_APPS.append("django_celery_beat")
+
+SITE_ID = env("SITE_ID", default=1)
+
+# Use rich logging for pretty console logs
+# https://www.willmcgugan.com/blog/tech/post/richer-django-logging/
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {"rich": {"datefmt": "[%X]"}},
+    "handlers": {
+        "console": {
+            "class": "rich.logging.RichHandler",
+            "formatter": "rich",
+            "level": "DEBUG",
+        }
+    },
+    "loggers": {"django": {"handlers": ["console"]}},
+}
+
+#######################################################################################
+# DEVELOPMENT SETTINGS
+#######################################################################################
+if DEBUG:
+    # SECURITY WARNING: don't run with debug turned on in production!
+
+    if importlib.util.find_spec("debug_toolbar"):
+        INSTALLED_APPS.append("debug_toolbar")
+        MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
+        INTERNAL_IPS = [
+            "127.0.0.1",
+        ]
+        # See also urls.py for debug_toolbar urls
--- ce/urls.py
+++ ce/urls.py
@@ -14,9 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("admin/doc/", include("django.contrib.admindocs.urls")),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    # NOTE: When DEBUG and staticfiles is installed, Django automatically adds static
+    # urls, but does not automatically serve MEDIA
+    from django.conf.urls.static import static
+
+    # Serve static and media files from development server
+    # urlpatterns += staticfiles_urlpatterns()  # automatic when DEBUG on
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    if "debug_toolbar" in settings.INSTALLED_APPS:
+        # Catch-all patterns may block these if appended, hence insert
+        urlpatterns.insert(0, path("__debug__/", include("debug_toolbar.urls")))
