83a3519a884f8065f2b5d83b1040639ea56f683c
Vince Veselosky <vince@control-escape.com>
2025-02-17 14:20:24 -0500

    Add sitemaps to please Googlebot

--- src/ce/settings.py
+++ src/ce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.redirects",
+    "django.contrib.sitemaps",
     "django.contrib.sites",
     "django.contrib.staticfiles",
     "sitevars",
